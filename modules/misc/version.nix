642c32f87cf56874a882ac4a891263659106a79d
Tristan Ross <tristan.ross@midstall.com>
2024-05-23 19:42:19 -0700

    feat(version): set getty prompt

--- modules/misc/version.nix
+++ modules/misc/version.nix
@@ -1,8 +1,14 @@
-{ lib, ... }:
+{ config, lib, options, ... }:
 with lib;
+let
+  isMobileNixOS = options ? mobile;
+in
 {
-  config.system.nixos = {
-    distroId = mkForce "expidus";
-    distroName = mkForce "ExpidusOS";
+  config = {
+    system.nixos = {
+      distroId = mkForce "expidus";
+      distroName = mkForce "ExpidusOS";
+    };
+    services.getty.greetingLine = mkDefault ''<<< Welcome to ExpidusOS ${config.system.nixos.label}${optionalString (isMobileNixOS) " on ${config.mobile.device.name}"} (\m) - \l >>>'';
   };
 }
