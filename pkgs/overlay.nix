d4f0549a7ba8d8183e9f5a3e0a08e5a93e91034d
Tristan Ross <tristan.ross@midstall.com>
2024-05-30 14:58:12 -0700

    fix: cross compiling dart and flutter

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,6 +1,11 @@
 final: prev:
 rec {
-  flutterPackages = prev.callPackages ./development/compilers/flutter {};
+  dart = prev.callPackage ./development/compilers/dart {};
+
+  flutterPackages = prev.callPackages ./development/compilers/flutter {
+    inherit dart;
+  };
+
   flutter = flutterPackages.stable;
 
   expidus = prev.expidus // (final.callPackages ./expidus {});
